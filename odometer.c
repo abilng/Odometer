78156feae5e529f1896a5cb8141e0d2ece376bd5
unknown <test@L-MAA-25004136.paypalcorp.com>
2015-07-22 16:46:55 +0530

    Functions added

--- odometer.c
+++ odometer.c
@@ -1,20 +1,59 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 
 #define MIN 1
 #define MAX 9
+#define BASE
 
 class Odometer {
 	
 	public:
-		int length;
+		int length, min, max;
 		Odometer(int);
 		bool isValidNumber(int)
 		int nextNumber(int);
 		int previousNumber(int);
 		int difference(int, int);
-}
+		void createDigitsArray(int &);
+		bool compare(int &, int);
+};
 
 Odometer::Odometer(int lenght) {
 	this->length = length;
+	min = max = 0;
+	for(int i = 1; i <= length; i++) {
+		min = (min + i) * BASE;
+	}
+	for(int i = BASE - length; i < BASE; i++) {
+		max = (max + i) * BASE;
+	}
+}
+
+void Odometer::createDigitsArray(int& digits) {
+	for(int i = length - 1; i >= 0; i--) {
+		digits[i] = number % 10;
+		if(digits[i] == 0) {
+			return false;
+		}
+		number /= 10;
+	}
+}
+
+bool Odometer::compare(int &digits, int copy_number) {
+	for(i = length - 1; i >= 0; i--) {
+		if(digits[i] != (copy_number % 10)) {
+			return false;
+		}
+		copy_number /= 10;
+	}
+	return true;
+}
+
+bool Odometer::isValidNumber(int number) {
+	int digits[10];
+	int copy_num = number;
+	createDigitsArray(digits);
+	sort(begin(digits), end(digits));
+	return compare(digits, copy_number);
 }
