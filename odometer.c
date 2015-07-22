deaca391ee6ee817f0e242f4132a014a7d7263ad
unknown <test@L-MAA-25004136.paypalcorp.com>
2015-07-22 18:02:22 +0530

    Done

--- odometer.c
+++ odometer.c
@@ -4,56 +4,98 @@
 
 #define MIN 1
 #define MAX 9
-#define BASE
+#define BASE 10
+
+using namespace std;
 
 class Odometer {
 	
 	public:
 		int length, min, max;
 		Odometer(int);
-		bool isValidNumber(int)
+		int getMinimum();
+		int getMaximum();
+		bool isValidNumber(int);
 		int nextNumber(int);
 		int previousNumber(int);
 		int difference(int, int);
-		void createDigitsArray(int &);
-		bool compare(int &, int);
+		void swap(int *, int *);
 };
 
-Odometer::Odometer(int lenght) {
-	this->length = length;
-	min = max = 0;
+int Odometer::getMinimum() {
+	int min;
 	for(int i = 1; i <= length; i++) {
 		min = (min + i) * BASE;
 	}
+	return min /= 10;
+}
+
+int Odometer::getMaximum() {
+	int max;
 	for(int i = BASE - length; i < BASE; i++) {
 		max = (max + i) * BASE;
 	}
+	return max /= 10;
 }
 
-void Odometer::createDigitsArray(int& digits) {
-	for(int i = length - 1; i >= 0; i--) {
-		digits[i] = number % 10;
-		if(digits[i] == 0) {
+Odometer::Odometer(int length) {
+	this->length = length;
+	min = getMinimum();
+	max = getMaximum();
+}
+
+bool Odometer::isValidNumber(int number) {
+	int previous = INT_MAX;
+	for(int i = 0; i < length; i++) {
+		if(previous <= number % 10) {
 			return false;
 		}
+		previous = number % 10;
 		number /= 10;
-	}
-}
-
-bool Odometer::compare(int &digits, int copy_number) {
-	for(i = length - 1; i >= 0; i--) {
-		if(digits[i] != (copy_number % 10)) {
-			return false;
-		}
-		copy_number /= 10;
 	}
 	return true;
 }
 
-bool Odometer::isValidNumber(int number) {
-	int digits[10];
-	int copy_num = number;
-	createDigitsArray(digits);
-	sort(begin(digits), end(digits));
-	return compare(digits, copy_number);
+int Odometer::previousNumber(int number){
+	int i = number <= min ? max : number - 1 ;
+	while (!isValidNumber(i)) {
+		i--;
+	}
+	return i;
+}
+
+int Odometer::nextNumber(int number) {
+	int i = number >= max ? min : number + 1 ;
+	while (!isValidNumber(i)) {
+		i++;
+	}
+	return i;
+}
+
+int Odometer::difference(int number1,int number2){
+	if (number1 > number2) {
+		swap(&number1, &number1);
+	}
+	int validCount = 0;
+	for (int i = number1 + 1 ; i < number2 ; i++ ) {
+		if (isValidNumber(i)) {
+			validCount++;
+		}
+	}
+	return validCount + 1;
+}
+
+void Odometer::swap(int *x,int *y){
+	int temp;
+	temp = *x;
+	*x = *y;
+	*y = temp;
+}
+
+int main() {
+	Odometer odometer(5);
+	cout << odometer.nextNumber(23456) << endl;
+	cout << odometer.previousNumber(23468) << endl;
+	cout << odometer.difference(23456, 35678) << endl;
+	return 0;
 }
